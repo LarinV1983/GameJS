c589a6c8e4eed0f0d159cfa0f9f382c815c2c087
Viktor Larin <Larinv1983@mail.ru>
2023-04-05 12:50:14 +0300

    изменение цвета плитки

--- tile.js
+++ tile.js
@@ -2,7 +2,7 @@ export class Tile {
 	constructor (gridElement ) {
 		this.tileElement = document.createElement('div');
 		this.tileElement.classList.add('tile');
-		this.value = Math.random() > 0.5 ? 2 : 4;
+		this.setValue(Math.random() > 0.5 ? 2 : 4);
 		this.tileElement.textContent = this.value;
 		gridElement.append(this.tileElement);
 	}
@@ -12,5 +12,13 @@ export class Tile {
 		this.y = y;
 		this.tileElement.style.setProperty('--x', x);
 		this.tileElement.style.setProperty('--y', y);
+	}
+
+	setValue(value){
+		this.value = value;
+		this.tileElement.textContent = value;
+		const bgLight = 100 - Math.log2(value) * 9;
+		this.tileElement.style.setProperty('--bg-Light', `${bgLight}%`);
+		this.tileElement.style.setProperty('--text-Light', `${bgLight < 50 ? 90 : 10}%`);
 	}
 } 
