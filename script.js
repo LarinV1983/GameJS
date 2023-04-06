e45a382568ab01317d1d2c2f82e99d6228d4c5f1
Viktor Larin <Larinv1983@mail.ru>
2023-04-06 10:54:37 +0300

    добавлена функция обрабтки клика

--- script.js
+++ script.js
@@ -6,3 +6,13 @@ const gameBoard = document.getElementById("game-board");
 const grid = new Grid(gameBoard);
 grid.getRandomCell().linkTile(new Tile(gameBoard));
 grid.getRandomCell().linkTile(new Tile(gameBoard));
+setupInput();
+
+function setupInput() {
+	window.addEventListener('keydown', handleInput, {once: true});
+	}
+
+	function handleInput(event) {
+		console.log(event.key);
+		setupInput();
+	}
