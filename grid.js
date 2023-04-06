56b8763b57399fc70f4179162a212b35792ff9fc
Viktor Larin <Larinv1983@mail.ru>
2023-04-06 11:56:46 +0300

    логика перемещения плитки вверх

--- grid.js
+++ grid.js
@@ -10,10 +10,20 @@ export class Grid {
 			this.cells.push(
 				new Cell(gridElement, i % GridSize, Math.floor(i / GridSize)))	  
 		}
+		this.cellsGroupColumn = this.cellsGroupColumn();
 	}
+
 	getRandomCell(){
 		const emptyCell = this.cells.filter(cell => cell.isEmpty());
 		const randomIndex = Math.floor(Math.random() * emptyCell.length);
 		return emptyCell[randomIndex];
+	}
+
+	cellsGroupColumn(){
+		return this.cells.reduce((groupCells, cell) => {
+			groupCells[cell.x] = groupCells[cell.x] || [];
+			groupCells[cell.x][cell.y] = cell;
+			return groupCells;
+		}, []);
 	}
 }
--- script.js
+++ script.js
@@ -13,6 +13,47 @@ function setupInput() {
 	}
 
 	function handleInput(event) {
-		console.log(event.key);
+		switch (event.key) {
+			case 'ArrowUp':
+				moveUp();
+			break;
+			case 'ArrowDown':
+				moveDown();
+			break;
+			case 'ArrowLeft':
+				moveLeft();
+			break;
+			case 'ArrowRight':
+				moveRight();
+			break;
+
+		default:
+			setupInput();
+			return;
+		}
+
 		setupInput();
+	}
+
+	function moveUp() {
+		slideTiles(grid.cellsGroupColumn);
+	}
+
+	function slideTiles(groupCells) {
+		groupCells.forEach(group => slideTitleGroup(group));
+	}
+
+	function slideTitleGroup(group) {
+		for (let i = 1; i< group.length; i++) {
+			if (group[i].isEmpty()) {
+				continue;
+			}
+			const cellTile = group[i];
+			let targetCell;
+			let j = i - 1;
+			if (!targetCell) {
+				continue;
+			}
+			
+		}
 	}
